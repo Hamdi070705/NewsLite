e0516113524b0de83d6d0dc004835fbec72a7ecb
Hamdi070705 <syaebatulhamdi07@gmail.com>
2025-06-10 16:48:28 +0800

    chore: add new dependencies in build.gradle.kts

--- NewsLiteApp/app/build.gradle.kts
+++ NewsLiteApp/app/build.gradle.kts
@@ -40,4 +40,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // Instal retrofit2
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Instal retrofit2-converter
+// OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.1") // Instal okhttp (caching, logging, dll)
+    implementation("com.squareup.picasso:picasso:2.71828") // Instal picasso (Menampilkan gambar)
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0") // Instal swipe refresh layout
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
